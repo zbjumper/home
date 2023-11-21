a0e344a370f2c4a83e133033ccde89be3d3ce5f8
Bill Zhang <zbjumper@gmail.com>
2023-11-21 11:50:09 +0800

    修复环境

--- ai-learning/.vitepress/config.mjs
+++ ai-learning/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "AI Learning",
   description: "A VitePress Site",
+  base: '/ai-learning/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
