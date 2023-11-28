21f9fc2fca2ea78f0d99ce18d85313a6aca99fd4
张博 <zbjumper@gmail.com>
2023-11-28 17:59:37 +0800

    补充 build脚本

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # /bin/bash
 
-docker build -t zbjumper/docs-storefront:0.0.2 --push storefront
+docker build -t zbjumper/docs-storefront:0.0.3 --push storefront
 
-# docker build -t zbjumper/docs-ai-learning:0.0.1 .
+docker build -t zbjumper/docs-ai-learning:0.0.2 --push ai-learning
 
 # docker build -t zbjumper/docs-vue3:0.0.2 .
