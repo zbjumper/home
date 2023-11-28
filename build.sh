11bb06546c718352203dc1fc695deeef0f6975c5
张博 <zbjumper@gmail.com>
2023-11-28 18:50:17 +0800

    增加图片缩放

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ docker build -t zbjumper/docs-ai-learning:0.0.2 --push ai-learning
 
 # docker build -t zbjumper/docs-vue3:0.0.2 .
 
-docker build -t zbjumper/docs-oxygen-not-included:0.0.1 --push oxygen-not-included
+docker build -t zbjumper/docs-oxygen-not-included:0.0.2 --push oxygen-not-included
